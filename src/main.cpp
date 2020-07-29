d5536af5ec03bce7bf853cf9dca50b3dd6b25d2e
aliismail92 <ali.ismail.sc@gmail.com>
2020-07-30 00:10:48 +0300

    Updated with PID for velocity control and ensure w

--- src/main.cpp
+++ src/main.cpp
@@ -33,8 +33,8 @@ const int PWM_B = 16;
 const int DIR_A = 17;
 const int DIR_B = 5;
 
-uint32_t freq = 4000;
-uint32_t resolution = 8;
+uint32_t freq = 5000;
+uint32_t resolution = 13;
 uint32_t MotorA = 0;
 uint32_t MotorB = 1;
 
@@ -71,8 +71,8 @@ int vel_r = 0;
 int vel_l = 0;
 
 //robot geometry
-const int wheel_base = 205.5;
-const int wheel_radius = 67;
+const float wheel_base = 205.5;
+const float wheel_radius = 33.5;
 
 int phi_current;
 int phi_des;
@@ -84,44 +84,55 @@ int phi_p = 0;
 
 //Target Location
 
-int x_g = 5;
-int y_g = 1;
+int x_g = 5000;
+int y_g = 5000;
 
 
-//PID terms
+//PID terms for phi
 
-const int Kp_phi = 2;
-const int Kd_phi = .4;
-const int Ki_phi = .1;
+const int Kp_phi = 2.5;
+const int Kd_phi = 0.5;
+const int Ki_phi = 0.2;
 
-int prev_d_phi = 0;
-int e_d_phi;
-int e_i_phi;
+float prev_d_phi = 0;
+float e_d_phi=0;
+float e_i_phi = 0;
 int sum_i_phi;
-const int limit_i_phi = 10;
+const int limit_i_phi = 1;
 
-const int Kp_v = 2;
-const int Kd_v = .6;
-const int Ki_v = .1;
+// PID terms for v
+const int Kp_v = 3;
+const int Kd_v = 10;
+const int Ki_v = 1.5;
 
-int prev_d_v = 0;
-int e_d_v;
-int e_i_v;
-int sum_i_v;
-const int limit_i_v = 20;
+float prev_d_v = 0;
+float e_d_v=0;
+float e_i_v = 0;
+int sum_i_v_r = 0;
+int sum_i_v_l = 0;
+const int limit_i_v = 500;
+
+int counter_r_pid = 0;
+int counter_l_pid = 0;
+int corrected_v_r = 0;
+int corrected_v_l = 0;
+
+unsigned long PID_millis_v = 0;
+unsigned long PID_millis_phi = 0;
 
 const int pi = 3.141592653989;
-long dt;
+float dt= 0.01;
 
 
 //Defining Custom Functions:
-void Move(int vel, int ang);
+void Move(float vel_r, float vel_l);
 void sense();
 void encode();
 void odom();
-void gotogoal();
+void gotogoal(int x, int y);
 void avoidObstacle();
 void followWall();
+void ensure_phi(int v, float w);
 
 
 
@@ -167,22 +178,18 @@ void setup() {
 
   // Set Motor to be off
 
-  /*digitalWrite(PWM_A, 0);
-  digitalWrite(PWM_B, 0);
-  delay(100);*/
+  ledcWrite(MotorA, 0);
+  ledcWrite(MotorB, 0);
+  delay(100);
 
   
 }
 
 void loop() {
 
-  
-  Move(500, 1);
-  encode();
-  Serial.print(counter_R);
-  Serial.print(" ");
-  Serial.println(counter_L);
-
+  x_g = 5000;
+  y_g = 5000;
+  gotogoal(x_g, y_g);
 
  
 }
@@ -215,7 +222,7 @@ void encode(){
 
 
 
-void Move(int vel_r, int vel_l){
+void Move(float vel_r, float vel_l){
 
   if (vel_r >= 0 ){
     digitalWrite(DIR_A, HIGH);
@@ -228,13 +235,41 @@ void Move(int vel_r, int vel_l){
   } else {
     digitalWrite(DIR_B, LOW);
   }
-  
 
-  digitalWrite(PWM_A, vel_r);
-  digitalWrite(PWM_B, vel_l);
+  float f_vel_r = vel_r;
+  float f_vel_l = vel_l;
+  unsigned long timer_pid_v = millis() - PID_millis_v;
+  PID_millis_v = timer_pid_v;
 
-  //ledcWrite(PWM_A, vel_r);
-  //ledcWrite(PWM_B, vel_l);
+  if (timer_pid_v >= 200){
+
+    float vel_est_r = (counter_R - counter_r_pid)/(823.0*timer_pid_v/1000.0)*2*pi;
+    float vel_est_l = (counter_L - counter_l_pid)/(823.0*timer_pid_v/1000.0)*2*pi;
+    float e_p_v_r = vel_r - vel_est_r;
+    float e_p_v_l = vel_l - vel_est_l;
+    sum_i_v_r = sum_i_v_r + e_p_v_r * timer_pid_v;
+    sum_i_v_l = sum_i_v_l + e_p_v_l * timer_pid_v;
+
+      if (sum_i_v_r > limit_i_v){
+        sum_i_v_r = limit_i_v;
+      }
+
+      if (sum_i_v_r > limit_i_v){
+        sum_i_v_r = limit_i_v;
+      }  
+
+    corrected_v_r = Kp_v * e_p_v_r + Ki_v * sum_i_v_r;
+    corrected_v_l = Kp_v * e_p_v_l + Ki_v * sum_i_v_l;
+
+  }
+  int u_v_r = f_vel_r + corrected_v_r;
+  int u_v_l = f_vel_l + corrected_v_l;
+
+  u_v_r = constrain(u_v_r, 0, 7500);
+  u_v_l = constrain(u_v_l, 0, 7500);
+
+  ledcWrite(MotorA, u_v_r);
+  ledcWrite(MotorB, u_v_l);
 }
 
 
@@ -253,52 +288,101 @@ void odom(){
 
 }
 
-void gotogoal(){
+void gotogoal(int x, int y){
 
 // Update position estimate
  odom();
 
 //Angle Errors
- int E_x = x_g - x_p;
- int E_y = y_g - y_p;
+ int E_x = x - x_p;
+ int E_y = y - y_p;
 
- int phi_g = atan2(E_y, E_x);
+ float phi_g = atan2(E_y, E_x);
 
- int E_g = phi_g - phi_p;
+ float E_g = phi_g - phi_p;
  E_g = atan2(sin(E_g), cos(E_g));
 
  e_i_phi = e_i_phi + E_g*dt;
  e_d_phi = (E_g - prev_d_phi)/dt;
+ prev_d_phi = E_g;
 
  if (e_i_phi > limit_i_phi){
    e_i_phi = limit_i_phi;
  }
  
- int u_phi = E_g * Kp_phi + e_i_phi * Ki_phi + e_d_phi * Kd_phi;
- 
+ int w = E_g * Kp_phi + e_i_phi * Ki_phi + e_d_phi * Kd_phi;
+
 //Distance Errors 
  
- int E_d = sqrt(E_x^2 +E_y^2);
- e_i_v = e_i_v + E_d *dt;
- e_d_v = (E_d - prev_d_v)/dt;
-
- if (e_i_v > limit_i_v){
-   e_i_v = limit_i_v;;
- }
-
- int u_v = E_d * Kp_v + e_i_v * Ki_v + e_d_v * Kd_v;
+ 
+ int v;
+ if (abs(E_g) > 1.22){
+   v = 0;
+ } else{
+   int mag = pow(E_x, 2) + pow(E_y, 2);
+   v = sqrt(mag);
+ } 
 
 //limiting V to ensure phi
+ ensure_phi(v, w);
 
-
-// Decomposing to indivicual wheel vel
-
- vel_r = (2*u_v + u_phi*wheel_base)/(2*wheel_radius);
- vel_l = (2*u_v - u_phi*wheel_base)/(2*wheel_radius);
+// Move
 
  Move(vel_r, vel_l);
+
+ Serial.print(v);
+ Serial.print(" ");
+ Serial.print(w);
+ Serial.print(" ");
+ Serial.print(vel_r);
+ Serial.print(" ");
+ Serial.println(vel_l);
 
 
 }
 
-//testing git with updated passward
+void ensure_phi(int v, float w){
+  
+  int vel_max = 21;
+  int vel_min = -21;
+  float w_int = w;
+
+  float w_lim_comp = ((wheel_radius/wheel_base)*(vel_max-vel_min));
+  float vel_lim = constrain(abs(v),(wheel_radius/2)*(2*vel_min), wheel_radius/2 * (2.0 * vel_max));
+  float w_lim = constrain( w_int, 0, w_lim_comp);
+  float vel_d_r = (2*vel_lim + w_lim*wheel_base)/(2*wheel_radius);
+  float vel_d_l = (2*vel_lim - w_lim*wheel_base)/(2*wheel_radius);
+  float vel_rl_max = max(vel_d_r, vel_d_l);
+  float vel_rl_min = min(vel_d_r, vel_d_l);
+
+  if (abs(v) > 0){
+    if (vel_rl_max > vel_max) {
+      vel_r  = vel_d_r - (vel_rl_max - vel_max);
+      vel_l = vel_d_l - (vel_rl_max - vel_max);
+    } else if(vel_rl_min <vel_min){
+      vel_r  = vel_d_r + (vel_min - vel_rl_min);
+      vel_l = vel_d_l + (vel_min - vel_rl_min);
+    } else {
+      vel_r = vel_d_r;
+      vel_l = vel_d_l;
+    }
+
+  }
+
+  else {
+    float w_min = wheel_radius/wheel_base *  (2 * vel_min);
+    float w_max = wheel_radius/wheel_base * (2* vel_max);
+
+    if (abs(w) > w_min) {
+      float abs_w = abs(w);
+      w =  constrain (abs_w, w_min, w_max);
+    } else {
+      w = 0;
+    }
+
+    vel_r = (w*wheel_base)/(2*wheel_radius);
+    vel_l = (-w*wheel_base)/(2*wheel_radius);
+
+  }
+  
+}
